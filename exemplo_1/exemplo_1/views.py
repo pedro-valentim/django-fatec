2483cf656e71c7999181100f2942c073c8e4b0b8
Pedro Valentim <pedro@znc.com.br>
2017-01-31 08:29:00 -0200

    Exemplo inicial - DjangoFatec - 30/01

--- exemplo_1/exemplo_1/views.py
+++ exemplo_1/exemplo_1/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
 import datetime
+from django.shortcuts import render
 
 
-def current_datetime(request):
+def home(request):
     nome = ''
     if request.method == 'POST':
         nome = request.POST.get('nome')
     now = datetime.datetime.now()
+    # html = "<html><body>Hello , %s! It is now %s.</body></html>" % (nome,now)
     return render(request, 'home.html', {'nome': nome, 'now': now})
--- exemplo_1/exemplo_1/wsgi.py
+++ exemplo_1/exemplo_1/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for exemplo project.
+WSGI config for exemplo_1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exemplo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exemplo_1.settings")
 
 application = get_wsgi_application()
